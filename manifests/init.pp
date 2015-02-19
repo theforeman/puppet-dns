0ea484bb51825ce981e38ea3c654236cff3c8c68
Ewoud Kohl van Wijngaarden <ekohlvanwijngaarden@antagonist.nl>
2015-02-19 17:00:09 +0100

    Coding style of init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,20 @@
 # Install, configure and start dns service
 class dns(
-  $namedconf_path = $dns::params::namedconf_path,
-  $dnsdir = $dns::params::dnsdir,
-  $dns_server_package = $dns::params::dns_server_package,
-  $rndckeypath = $dns::params::rndckeypath,
-  $optionspath = $dns::params::optionspath,
-  $publicviewpath = $dns::params::publicviewpath,
-  $vardir = $dns::params::vardir,
-  $namedservicename = $dns::params::namedservicename,
-  $zonefilepath = $dns::params::zonefilepath,
-  $localzonepath = $dns::params::localzonepath,
-  $forwarders = $dns::params::forwarders,
-  $listen_on_v6 = $dns::params::listen_on_v6
+  $namedconf_path     = $::dns::params::namedconf_path,
+  $dnsdir             = $::dns::params::dnsdir,
+  $dns_server_package = $::dns::params::dns_server_package,
+  $rndckeypath        = $::dns::params::rndckeypath,
+  $optionspath        = $::dns::params::optionspath,
+  $publicviewpath     = $::dns::params::publicviewpath,
+  $vardir             = $::dns::params::vardir,
+  $namedservicename   = $::dns::params::namedservicename,
+  $zonefilepath       = $::dns::params::zonefilepath,
+  $localzonepath      = $::dns::params::localzonepath,
+  $forwarders         = $::dns::params::forwarders,
+  $listen_on_v6       = $::dns::params::listen_on_v6
 ) inherits dns::params {
-  class { 'dns::install': } ~>
-  class { 'dns::config': } ~>
-  class { 'dns::service': } ->
+  class { '::dns::install': } ~>
+  class { '::dns::config': } ~>
+  class { '::dns::service': } ->
   Class['dns']
 }
