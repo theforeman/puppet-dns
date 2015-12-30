aad7f32cc0e4b68ea6e95cfe00ffca64edbca068
Jeremy Mathevet <jeremy.mathevet@net-a-porter.com>
2015-12-30 17:48:00 +0000

    Add spec tests

--- spec/classes/dns_init_spec.rb
+++ spec/classes/dns_init_spec.rb
@@ -46,7 +46,17 @@ describe 'dns' do
                   with_content(%r{empty-zones-enable no;}) }
     end
 
-    describe 'with service_ensure stopped' do
+    describe 'with forward only' do
+      let(:params) { {:forward => 'only'} }
+      it { should contain_file('/etc/named/options.conf').with_content(%r{forward only;}) }
+    end
+
+    describe 'with forward first' do
+      let(:params) { {:forward => 'first'} }
+      it { should contain_file('/etc/named/options.conf').with_content(%r{forward first;}) }
+    end
+
+      describe 'with service_ensure stopped' do
       let(:params) { {:service_ensure => 'stopped'} }
       it { should contain_service('named').with_ensure('stopped').with_enable(true) }
     end
