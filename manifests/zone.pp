1807e0cd424caaf64f5c334e79fda2f04dc16a07
Trey Dockendorf <treydock@gmail.com>
2016-01-20 11:29:50 -0600

    Add dns_notify to dns class and dns::zone define
    closes GH-54

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -20,13 +20,23 @@ define dns::zone (
     $manage_file    = true,
     $forward        = 'first',
     $forwarders     = [],
+    $dns_notify     = undef,
 ) {
 
   validate_bool($reverse, $manage_file)
   validate_array($masters, $allow_transfer, $forwarders, $also_notify)
   validate_re($forward, '^(first|only)$', 'Only \'first\' or \'only\' are valid values for forward field')
+  if $dns_notify {
+    validate_re($dns_notify, '^(yes|no|explicit)$', 'Only \'yes\', \'no\', or \'explicit\' are valid values for dns_notify field')
+  }
 
   $zonefilename = "${zonefilepath}/${filename}"
+
+  if $zonetype == 'slave' {
+    $_dns_notify = pick($dns_notify, 'no')
+  } else {
+    $_dns_notify = $dns_notify
+  }
 
   concat::fragment { "dns_zones+10_${zone}.dns":
     target  => $::dns::publicviewpath,
