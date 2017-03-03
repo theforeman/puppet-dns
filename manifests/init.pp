fa16badf57a45763da6e500f66f15d7daf1e5563
Tim Meusel <tim@bastelfreak.de>
2017-03-03 13:26:30 +0100

    replace validate_* in dns::init with datatypes

--- manifests/init.pp
+++ manifests/init.pp
@@ -101,53 +101,35 @@
 #     include dns
 #
 class dns (
-  $namedconf_path        = $::dns::params::namedconf_path,
-  $dnsdir                = $::dns::params::dnsdir,
-  $dns_server_package    = $::dns::params::dns_server_package,
-  $rndckeypath           = $::dns::params::rndckeypath,
-  $optionspath           = $::dns::params::optionspath,
-  $publicviewpath        = $::dns::params::publicviewpath,
-  $vardir                = $::dns::params::vardir,
-  $namedservicename      = $::dns::params::namedservicename,
-  $zonefilepath          = $::dns::params::zonefilepath,
-  $localzonepath         = $::dns::params::localzonepath,
-  $forward               = $::dns::params::forward,
-  $forwarders            = $::dns::params::forwarders,
-  $listen_on_v6          = $::dns::params::listen_on_v6,
-  $recursion             = $::dns::params::recursion,
-  $allow_recursion       = $::dns::params::allow_recursion,
-  $allow_query           = $::dns::params::allow_query,
-  $empty_zones_enable    = $::dns::params::empty_zones_enable,
-  $dns_notify            = $::dns::params::dns_notify,
-  $dnssec_enable         = $::dns::params::dnssec_enable,
-  $dnssec_validation     = $::dns::params::dnssec_validation,
-  $namedconf_template    = $::dns::params::namedconf_template,
-  $acls                  = $::dns::params::acls,
-  $optionsconf_template  = $::dns::params::optionsconf_template,
-  $controls              = $::dns::params::controls,
-  $service_ensure        = $::dns::params::service_ensure,
-  $service_enable        = $::dns::params::service_enable,
-  $additional_options    = $::dns::params::additional_options,
-  $additional_directives = $::dns::params::additional_directives,
+  Stdlib::Absolutepath $namedconf_path                            = $::dns::params::namedconf_path,
+  Stdlib::Absolutepath $dnsdir                                    = $::dns::params::dnsdir,
+  String $dns_server_package                                      = $::dns::params::dns_server_package,
+  Stdlib::Absolutepath $rndckeypath                               = $::dns::params::rndckeypath,
+  Stdlib::Absolutepath $optionspath                               = $::dns::params::optionspath,
+  Stdlib::Absolutepath $publicviewpath                            = $::dns::params::publicviewpath,
+  Stdlib::Absolutepath $vardir                                    = $::dns::params::vardir,
+  String $namedservicename                                        = $::dns::params::namedservicename,
+  Stdlib::Absolutepath $zonefilepath                              = $::dns::params::zonefilepath,
+  Optional[Stdlib::Absolutepath] $localzonepath                   = $::dns::params::localzonepath,
+  Optional[Enum['only', 'first']] $forward                        = $::dns::params::forward,
+  Array[String] $forwarders                                       = $::dns::params::forwarders,
+  Optional[Variant[String, Boolean]] $listen_on_v6                = $::dns::params::listen_on_v6,
+  Enum['yes', 'no'] $recursion                                    = $::dns::params::recursion,
+  Array[String] $allow_recursion                                  = $::dns::params::allow_recursion,
+  Array[String] $allow_query                                      = $::dns::params::allow_query,
+  Enum[yes, no] $empty_zones_enable                               = $::dns::params::empty_zones_enable,
+  Optional[Enum['yes', 'no', 'explicit']] $dns_notify             = $::dns::params::dns_notify,
+  Enum['yes', 'no'] $dnssec_enable                                = $::dns::params::dnssec_enable,
+  Enum['yes', 'no', 'auto'] $dnssec_validation                    = $::dns::params::dnssec_validation,
+  String $namedconf_template                                      = $::dns::params::namedconf_template,
+  Hash[String, Array[String]] $acls                               = $::dns::params::acls,
+  String $optionsconf_template                                    = $::dns::params::optionsconf_template,
+  Hash[String, Hash[String, Data]] $controls                      = $::dns::params::controls,
+  Variant[Enum['running', 'stopped'], Boolean] $service_ensure    = $::dns::params::service_ensure,
+  Boolean $service_enable                                         = $::dns::params::service_enable,
+  Hash $additional_options                                        = $::dns::params::additional_options,
+  Array $additional_directives                                    = $::dns::params::additional_directives,
 ) inherits dns::params {
-  validate_array($dns::forwarders)
-  validate_array($dns::allow_recursion)
-  validate_array($dns::allow_query)
-  validate_re($dns::recursion, '^(yes|no)$', 'Only \'yes\' and \'no\' are valid values for recursion field')
-  validate_re($dns::dnssec_enable, '^(yes|no)$', 'Only \'yes\' and \'no\' are valid values for dnssec_enable field')
-  validate_re($dns::dnssec_validation, '^(yes|no|auto)$', 'Only \'yes\', \'no\' and \'auto\' are valid values for dnssec_validation field')
-  validate_re($dns::service_ensure, '^running|true|stopped|false$', 'Only \'running\', \'false\', \'stopped\' and \'false\' are validate values for service_ensure field')
-  if $dns::forward {
-    validate_re($dns::forward, '^(only|first)$', 'Only \'only\' and \'first\' are valid values for forward field')
-  }
-  if $dns::dns_notify {
-    validate_re($dns_notify, '^(yes|no|explicit)$', 'Only \'yes\', \'no\', or \'explicit\' are valid values for dns_notify field')
-  }
-  validate_bool($dns::service_enable)
-  validate_hash($controls)
-  validate_hash($acls)
-  validate_hash($additional_options)
-  validate_array($additional_directives)
 
   class { '::dns::install': }
   ~> class { '::dns::config': }
