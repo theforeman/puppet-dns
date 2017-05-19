9a73bcc21819f769f0a5bce9d4242a97a90322d2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-05-19 15:44:04 +0200

    Consistent indenting

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,94 +1,99 @@
 # Default parameters
 class dns::params {
-    case $::osfamily {
-      'Debian': {
-        $dnsdir             = '/etc/bind'
-        $vardir             = '/var/cache/bind'
-        $optionspath        = "${dnsdir}/named.conf.options"
-        $zonefilepath       = "${vardir}/zones"
-        $localzonepath      = "${dnsdir}/zones.rfc1918"
-        $publicviewpath     = "${dnsdir}/zones.conf"
-        $dns_server_package = 'bind9'
-        $namedservicename   = 'bind9'
-        $user               = 'bind'
-        $group              = 'bind'
-        $rndcconfgen        = '/usr/sbin/rndc-confgen'
-      }
-      'RedHat': {
-        $dnsdir             = '/etc'
-        $vardir             = '/var/named'
-        $optionspath        = '/etc/named/options.conf'
-        $zonefilepath       = "${vardir}/dynamic"
-        $localzonepath      = "${dnsdir}/named.rfc1912.zones"
-        $publicviewpath     = "${dnsdir}/named/zones.conf"
-        $dns_server_package = 'bind'
-        $namedservicename   = 'named'
-        $user               = 'named'
-        $group              = 'named'
-        $rndcconfgen        = '/usr/sbin/rndc-confgen'
-      }
-      /^(FreeBSD|DragonFly)$/: {
-        $dnsdir             = '/usr/local/etc/namedb'
-        $vardir             = '/usr/local/etc/namedb/working'
-        $optionspath        = '/usr/local/etc/namedb/options.conf'
-        $zonefilepath       = "${dnsdir}/dynamic"
-        $localzonepath      = undef # "${dnsdir}/master/empty.db"
-        $publicviewpath     = "${dnsdir}/zones.conf"
-        $dns_server_package = 'bind910'
-        $namedservicename   = 'named'
-        $user               = 'bind'
-        $group              = 'bind'
-        $rndcconfgen        = '/usr/local/sbin/rndc-confgen'
-      }
-      'Archlinux': {
-        $dnsdir             = '/etc'
-        $vardir             = '/var/named'
-        $optionspath        = '/etc/named.options.conf'
-        $zonefilepath       = "${vardir}/dynamic"
-        $localzonepath      = undef # "${dnsdir}/named.local.conf"
-        $publicviewpath     = "${dnsdir}/zones.conf"
-        $dns_server_package = 'bind'
-        $namedservicename   = 'named'
-        $user               = 'named'
-        $group              = 'named'
-        $rndcconfgen        = '/usr/sbin/rndc-confgen'
-      }
-      default: {
-        fail ("Unsupported operating system family ${::osfamily}")
-      }
+  case $::osfamily {
+    'Debian': {
+      $dnsdir             = '/etc/bind'
+      $vardir             = '/var/cache/bind'
+      $optionspath        = "${dnsdir}/named.conf.options"
+      $zonefilepath       = "${vardir}/zones"
+      $localzonepath      = "${dnsdir}/zones.rfc1918"
+      $publicviewpath     = "${dnsdir}/zones.conf"
+      $dns_server_package = 'bind9'
+      $namedservicename   = 'bind9'
+      $user               = 'bind'
+      $group              = 'bind'
+      $rndcconfgen        = '/usr/sbin/rndc-confgen'
     }
+    'RedHat': {
+      $dnsdir             = '/etc'
+      $vardir             = '/var/named'
+      $optionspath        = '/etc/named/options.conf'
+      $zonefilepath       = "${vardir}/dynamic"
+      $localzonepath      = "${dnsdir}/named.rfc1912.zones"
+      $publicviewpath     = "${dnsdir}/named/zones.conf"
+      $dns_server_package = 'bind'
+      $namedservicename   = 'named'
+      $user               = 'named'
+      $group              = 'named'
+      $rndcconfgen        = '/usr/sbin/rndc-confgen'
+    }
+    /^(FreeBSD|DragonFly)$/: {
+      $dnsdir             = '/usr/local/etc/namedb'
+      $vardir             = '/usr/local/etc/namedb/working'
+      $optionspath        = '/usr/local/etc/namedb/options.conf'
+      $zonefilepath       = "${dnsdir}/dynamic"
+      $localzonepath      = undef # "${dnsdir}/master/empty.db"
+      $publicviewpath     = "${dnsdir}/zones.conf"
+      $dns_server_package = 'bind910'
+      $namedservicename   = 'named'
+      $user               = 'bind'
+      $group              = 'bind'
+      $rndcconfgen        = '/usr/local/sbin/rndc-confgen'
+    }
+    'Archlinux': {
+      $dnsdir             = '/etc'
+      $vardir             = '/var/named'
+      $optionspath        = '/etc/named.options.conf'
+      $zonefilepath       = "${vardir}/dynamic"
+      $localzonepath      = undef # "${dnsdir}/named.local.conf"
+      $publicviewpath     = "${dnsdir}/zones.conf"
+      $dns_server_package = 'bind'
+      $namedservicename   = 'named'
+      $user               = 'named'
+      $group              = 'named'
+      $rndcconfgen        = '/usr/sbin/rndc-confgen'
+    }
+    default: {
+      fail ("Unsupported operating system family ${::osfamily}")
+    }
+  }
 
-    $namedconf_template    = 'dns/named.conf.erb'
-    $optionsconf_template  = 'dns/options.conf.erb'
+  $namedconf_template    = 'dns/named.conf.erb'
+  $optionsconf_template  = 'dns/options.conf.erb'
 
-    $namedconf_path        = "${dnsdir}/named.conf"
+  $namedconf_path        = "${dnsdir}/named.conf"
 
-    #pertaining to rndc
-    $rndckeypath           = "${dnsdir}/rndc.key"
+  #pertaining to rndc
+  $rndckeypath           = "${dnsdir}/rndc.key"
 
-    $forward               = undef
-    $forwarders            = []
+  $forward               = undef
+  $forwarders            = []
 
-    $listen_on_v6          = 'any'
+  $listen_on_v6          = 'any'
 
-    $recursion             = 'yes'
-    $allow_recursion       = [ 'localnets', 'localhost' ]
-    $allow_query           = [ 'any' ]
+  $recursion             = 'yes'
+  $allow_recursion       = [ 'localnets', 'localhost' ]
+  $allow_query           = [ 'any' ]
 
-    $empty_zones_enable    = 'yes'
+  $empty_zones_enable    = 'yes'
 
-    $dns_notify            = undef
+  $dns_notify            = undef
 
-    $dnssec_enable         = 'yes'
-    $dnssec_validation     = 'yes'
+  $dnssec_enable         = 'yes'
+  $dnssec_validation     = 'yes'
 
-    $controls              = { '127.0.0.1' => { 'port' => 953, 'allowed_addresses' => [ '127.0.0.1' ], 'keys' => [ 'rndc-key' ] }, }
+  $controls              = {
+    '127.0.0.1' => {
+      'port' => 953,
+      'allowed_addresses' => [ '127.0.0.1' ],
+      'keys' => [ 'rndc-key' ],
+    },
+  }
 
-    $service_ensure        = 'running'
-    $service_enable        = true
-    $acls                  = {}
+  $service_ensure        = 'running'
+  $service_enable        = true
+  $acls                  = {}
 
-    $additional_options    = {}
-    $additional_directives = []
-
+  $additional_options    = {}
+  $additional_directives = []
 }
