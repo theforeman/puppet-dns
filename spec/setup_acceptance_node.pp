afee3b0f26bc2a18370033cec003835a5a7c0dbe
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-11-04 13:06:48 +0100

    Drop Fedora 26, add Fedora 32
    Currently our testing infra doesn't support Fedora 33 yet so it's not
    added.

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -7,3 +7,10 @@ $package = $facts['os']['family'] ? {
 package { $package:
   ensure => installed,
 }
+
+# Needed for the ss command in testing and not included in the base container
+if $facts['os']['name'] == 'Fedora' {
+  package { 'iproute':
+    ensure => installed,
+  }
+}
