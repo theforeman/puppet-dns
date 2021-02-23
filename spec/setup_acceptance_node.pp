43e446a9d517a336b07b0304633bb1380d0422fc
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-02-23 16:13:42 +0100

    Update Github Actions workflow
    This applies the lessons learned while applying this to Voxpupuli and
    The Foreman orgs.

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -8,9 +8,14 @@ package { $package:
   ensure => installed,
 }
 
-# Needed for the ss command in testing and not included in the base container
+# Needed for the ss and sysctl commands in testing and not included in the base container
 if $facts['os']['name'] == 'Fedora' {
-  package { 'iproute':
+  package { ['iproute', 'procps-ng']:
     ensure => installed,
   }
+}
+
+# The both IPv4 and IPv6 are used to test views
+exec { 'sysctl -w net.ipv6.conf.all.disable_ipv6=0':
+  path => ['/usr/sbin', '/sbin', '/usr/bin', '/bin'],
 }
