theforeman/puppet-dns

commit cc80d3caa6fbb8e7aa3b79aaa8d049d2d443324e

Handle dnssec-enable config option removal
In BIND 9.15 the dnssec-enable config option was made obsolete (it's
always enabled) and 9.18 removed it.

manifests/params.pp
@@ -24,6 +24,12 @@ class dns::params {
 
       # This option is not relevant for Debian
       $sysconfig_disable_zone_checking = undef
+
+      $dnssec_enable = $facts['os']['name'] ? {
+        'Debian' => if versioncmp($facts['os']['release']['major'], '11') >= 0 { undef } else { 'yes' },
+        'Ubuntu' => if versioncmp($facts['os']['release']['major'], '20.04') >= 0 { undef } else { 'yes' },
+        default  => undef,
+      }
     }
     'RedHat': {
       $dnsdir             = '/etc'
@@ -47,6 +53,8 @@ class dns::params {
 
       # This option is not relevant for RedHat
       $sysconfig_resolvconf_integration = undef
+
+      $dnssec_enable = 'yes'
     }
     /^(FreeBSD|DragonFly)$/: {
       $dnsdir             = '/usr/local/etc/namedb'
@@ -69,6 +77,7 @@ class dns::params {
       $sysconfig_startup_options = undef
       $sysconfig_disable_zone_checking = undef
       $sysconfig_resolvconf_integration = undef
+      $dnssec_enable = undef
     }
     'Archlinux': {
       $dnsdir             = '/etc'
@@ -91,6 +100,8 @@ class dns::params {
       $sysconfig_startup_options = undef
       $sysconfig_disable_zone_checking = undef
       $sysconfig_resolvconf_integration = undef
+
+      $dnssec_enable = undef
     }
     default: {
       fail ("Unsupported operating system family ${facts['os']['family']}")

spec/classes/dns_init_spec.rb
@@ -100,16 +100,25 @@ describe 'dns' do
 
         it { should contain_concat(options_path) }
         it do
+          has_dnssec_enable = case facts[:os]['family']
+                              when 'Debian'
+                                ['9', '10', '18.04'].include?(facts[:os]['release']['major'])
+                              when 'RedHat'
+                                true
+                              else
+                                false
+                              end
           expected = [
             "directory \"#{var_path}\";",
             'recursion yes;',
             'allow-query { any; };',
-            'dnssec-enable yes;',
             'dnssec-validation yes;',
             'empty-zones-enable yes;',
             'listen-on-v6 { any; };',
             'allow-recursion { localnets; localhost; };'
           ]
+
+          expected << 'dnssec-enable yes;' if has_dnssec_enable
 
           if facts[:os]['family'] == 'FreeBSD'
             expected << 'pid-file "/var/run/named/pid";'

manifests/init.pp
@@ -56,7 +56,8 @@
 # @param dns_notify
 #   The notify option in named.conf
 # @param dnssec_enable
-#   The dnssec-enable option
+#   The dnssec-enable option. This option is deprecated and has no effect since
+#   BIND 9.15. It's been removed in BIND 9.18.
 # @param dnssec_validation
 #   The dnssec-validation option
 # @param namedconf_template
@@ -157,7 +158,7 @@ class dns (
   Array[String] $allow_query                                        = ['any'],
   Enum['yes', 'no'] $empty_zones_enable                             = 'yes',
   Optional[Enum['yes', 'no', 'explicit']] $dns_notify               = undef,
-  Enum['yes', 'no'] $dnssec_enable                                  = 'yes',
+  Optional[Enum['yes', 'no']] $dnssec_enable                        = $dns::params::dnssec_enable,
   Enum['yes', 'no', 'auto'] $dnssec_validation                      = 'yes',
   String $namedconf_template                                        = 'dns/named.conf.erb',
   Hash[String, Array[String]] $acls                                 = {},

templates/options.conf.erb
@@ -8,7 +8,9 @@ forward <%= scope.lookupvar('::dns::forward') %>;
 
 recursion <%= scope.lookupvar('::dns::recursion') %>;
 allow-query { <%= scope.lookupvar('::dns::allow_query').join("; ") %>; };
+<% unless [false, nil, :undefined, :undef, ''].include?(scope.lookupvar('::dns::dnssec_enable')) -%>
 dnssec-enable <%= scope.lookupvar('::dns::dnssec_enable') %>;
+<% end -%>
 dnssec-validation <%= scope.lookupvar('::dns::dnssec_validation') %>;
 
 empty-zones-enable <%= scope.lookupvar('::dns::empty_zones_enable') %>;

commit 9db91f1a5bc229ce3eb6b0956f0844c1cd717031

Remove dnssec-enable option for EL >= 9 (#218)
EL9 ships with a version of BIND where dnssec-enable is invalid. In
cc80d3caa6fbb8e7aa3b79aaa8d049d2d443324e it was made optional, but forgot EL9.

Fixes: cc80d3caa6fbb8e7aa3b79aaa8d049d2d443324e
Co-authored-by: Matt Darcy <mattd@dolly.no-dns.co.uk>

manifests/params.pp
@@ -54,7 +54,7 @@ class dns::params {
       # This option is not relevant for RedHat
       $sysconfig_resolvconf_integration = undef
 
-      $dnssec_enable = 'yes'
+      $dnssec_enable = if versioncmp($facts['os']['release']['major'], '9') >= 0 { undef } else { 'yes' }
     }
     /^(FreeBSD|DragonFly)$/: {
       $dnsdir             = '/usr/local/etc/namedb'

spec/classes/dns_init_spec.rb
@@ -111,7 +111,7 @@ describe 'dns' do
                               when 'Debian'
                                 ['9', '10', '18.04'].include?(facts[:os]['release']['major'])
                               when 'RedHat'
-                                true
+                                ['7', '8'].include?(facts[:os]['release']['major'])
                               else
                                 false
                               end