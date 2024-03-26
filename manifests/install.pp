2bde91cc25ac6d05d21ff436143d7e3952abc102
Tim Meusel <tim@bastelfreak.de>
2024-03-26 16:10:25 +0100

    Update ensure_packages->stdlib::ensure_packages; require stdlib 9
    The `ensure_packages()` function is deprecated with stdlib 9. It
    triggers a catalog compilation warning on Puppet 7 and a catgalog
    compilation error on Pupppet 8. This PR switches to the successor,
    `stdlib::ensure_packages()`. This function got introduced in stdlib 9.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@
 # @api private
 class dns::install {
   if ! empty($dns::dns_server_package) {
-    ensure_packages([$dns::dns_server_package])
+    stdlib::ensure_packages([$dns::dns_server_package])
     $pkg_req = Package[$dns::dns_server_package]
   } else {
     $pkg_req = undef
