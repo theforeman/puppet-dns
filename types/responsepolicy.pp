9c9f14f483c13a62981045146f44feda3537c08c
supun983 <51007851+supun983@users.noreply.github.com>
2024-11-28 13:39:33 +0530

    Update types/responsepolicy.pp
    Implementing as suggested
    
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- types/responsepolicy.pp
+++ types/responsepolicy.pp
@@ -5,7 +5,7 @@ type Dns::ResponsePolicy = Array[
       'given', 'disabled', 'passthru', 'drop',
       'nxdomain', 'nodata', 'tcp-only', 'cname'
     ]],
-    cname_domain   => Optional[String[1]],
+    cname_domain   => Optional[Stdlib::Fqdn],
     max_policy_ttl => Optional[Integer[0]],
     log            => Optional[Boolean]
   }], 1, 32
